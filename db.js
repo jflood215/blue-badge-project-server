60f1fd8a7cd13d5e66bef2d372de7f40c7f78db2
John Flood <jflood215@gmail.com>
2021-03-31 13:26:58 -0400

    updated db.js for deployment

--- db.js
+++ db.js
@@ -1,12 +1,13 @@
 const Sequelize = require("sequelize");
 
-// const sequelize = new Sequelize('blue-badge-server', 'postgres', 'password', {
-//     host: 'localhost',
-//     dialect: 'postgres'
-// });
-
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+        require: true,
+        rejectUnauthorized: false, // very important
+      }
+  }
 });
 
 try {
